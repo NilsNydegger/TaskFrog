2bbf66502b8218574b9f418c29efbd2f35627fb9
SgtSigu <fabian.siegenthaler@students.ffhs.ch>
2022-05-02 15:10:50 +0200

    Added button *quack*

--- app/src/main/java/com/example/taskfrog/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/taskfrog/ui/calendar/CalendarFragment.kt
@@ -1,4 +1,4 @@
-package com.example.taskfrog.ui.notifications
+package com.example.taskfrog.ui.calendar
 
 import android.os.Bundle
 import android.view.LayoutInflater
--- app/src/main/java/com/example/taskfrog/ui/calendar/CalendarViewModel.kt
+++ app/src/main/java/com/example/taskfrog/ui/calendar/CalendarViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.taskfrog.ui.notifications
+package com.example.taskfrog.ui.calendar
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
