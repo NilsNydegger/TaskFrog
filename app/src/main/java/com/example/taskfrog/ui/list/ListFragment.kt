8366fff6b1262c627f7100a628773ca1756214ec
NilsNydegger <nils.nydegger@students.ffhs.ch>
2022-05-02 13:55:42 +0200

    kero kero renamed stuff kero

--- app/src/main/java/com/example/taskfrog/ui/list/ListFragment.kt
+++ app/src/main/java/com/example/taskfrog/ui/list/ListFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.taskfrog.databinding.FragmentDashboardBinding
+import com.example.taskfrog.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentListBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class ListFragment : Fragment() {
         val dashboardViewModel =
             ViewModelProvider(this).get(ListViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
