c4862d3a96c756d007e5e3f255fcf9b720b89227
NilsNydegger <nils.nydegger@students.ffhs.ch>
2022-05-09 15:47:20 +0200

    forg (I gotta save real quick before trying something... incredible)

---
+++ app/src/main/java/com/example/taskfrog/room/Converters.kt
@@ -0,0 +1,12 @@
+package com.example.taskfrog.room
+
+import androidx.room.*
+
+class Converters {
+
+    /*@TypeConverter
+    fun fromStringToArrayList(value: String): ArrayList<>{
+
+    }*/
+
+}
