93a02dc41e58b71c07d4451b035ac2bef69d365e
SgtSigu <fabian.siegenthaler@students.ffhs.ch>
2022-05-09 14:45:11 +0200

    added items

--- app/src/main/java/com/example/taskfrog/MainActivity.kt
+++ app/src/main/java/com/example/taskfrog/MainActivity.kt
@@ -1,21 +1,13 @@
 package com.example.taskfrog
 
-import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.taskfrog.databinding.ActivityMainBinding
-import com.example.taskfrog.db.FroggyList
-import com.example.taskfrog.ui.list.FroggyListAdapter
-import com.example.taskfrog.ui.list.FroggyListViewModel
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     private var mFroggyViewModel: FroggyListViewModel? = null
---
+++ app/src/main/java/com/example/taskfrog/ui/list/ListItemsViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.taskfrog.ui.list
+
+data class ListItemsViewModel(val text: String) {
+}
