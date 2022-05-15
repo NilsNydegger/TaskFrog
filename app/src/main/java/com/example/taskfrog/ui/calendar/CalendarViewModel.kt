f354c57e499bcc92becf3acaa9771ccae802dbb9
SgtSigu <fabian.siegenthaler@students.ffhs.ch>
2022-05-15 18:10:51 +0200

    Fixed EditTask functionality Added Icon to Select Due Date Button Changed Text on Home and Calendar Fragments

--- app/src/main/java/com/example/taskfrog/ui/calendar/CalendarViewModel.kt
+++ app/src/main/java/com/example/taskfrog/ui/calendar/CalendarViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class CalendarViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is Calendar Fragment"
+        value = "Calendar Fragment coming soon!"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/example/taskfrog/ui/list/TaskAdapter.kt
+++ app/src/main/java/com/example/taskfrog/ui/list/TaskAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.taskfrog.ui.list
 
 import android.app.AlertDialog
+import android.app.DatePickerDialog
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -9,9 +10,14 @@ import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
 import com.example.taskfrog.R
 import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 import kotlin.collections.ArrayList
 
 class TaskAdapter(val c: Context, val mTask: ArrayList<TaskData>) : RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
+
+    private lateinit var date : String
+    var cal = Calendar.getInstance()
 
     inner class TaskViewHolder(vTask: View) : RecyclerView.ViewHolder(vTask) {
         var taskName: TextView
@@ -32,20 +38,36 @@ class TaskAdapter(val c: Context, val mTask: ArrayList<TaskData>) : RecyclerView
         private fun popupMenus(v: View) {
             val position = mTask[adapterPosition]
             val popupMenus = PopupMenu(c, v)
+
             popupMenus.inflate(R.menu.show_menu_task)
             popupMenus.setOnMenuItemClickListener {
                 when (it.itemId) {
                     R.id.editText -> {
                         val vTask = LayoutInflater.from(c).inflate(R.layout.add_task, null)
                         val name = vTask.findViewById<EditText>(R.id.taskName)
-                        val date = vTask.findViewById<EditText>(R.id.taskDate)
+                        val dueDate = vTask.findViewById<Button>(R.id.taskDate)
                         val description = vTask.findViewById<EditText>(R.id.taskDescription)
+
+                        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+                            override fun onDateSet(view: DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+                                cal.set(Calendar.YEAR, year)
+                                cal.set(Calendar.MONTH, monthOfYear)
+                                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                                updateDateInView()
+                            }
+                        }
+                        dueDate!!.setOnClickListener(object : View.OnClickListener {
+                            override fun onClick(view : View) {
+                                DatePickerDialog(c, dateSetListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
+                            }
+                        })
+
                             AlertDialog.Builder(c)
                                 .setView(vTask)
                                 .setPositiveButton("Ok") {
                                     dialog,_->
                                     position.name = name.text.toString()
-                                    position.dueDate = DateFormat.getDateInstance().format(date)
+                                    position.dueDate = date
                                     position.description = description.text.toString()
                                     notifyDataSetChanged()
                                     Toast.makeText(c, "Task Edited", Toast.LENGTH_SHORT).show()
@@ -113,4 +135,9 @@ class TaskAdapter(val c: Context, val mTask: ArrayList<TaskData>) : RecyclerView
         return mTask.size
     }
 
+    private fun updateDateInView() {
+        val myFormat = "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.GERMAN)
+        date = sdf.format(cal.getTime())
+    }
 }
