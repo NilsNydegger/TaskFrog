de18b31f4fb5fded062db8e2334d06652295da17
SgtSigu <fabian.siegenthaler@students.ffhs.ch>
2022-06-07 17:10:34 +0200

    Added addList() test method

--- app/src/androidTest/java/com/example/taskfrog/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/taskfrog/ExampleInstrumentedTest.kt
@@ -1,5 +1,11 @@
 package com.example.taskfrog
 
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +13,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +22,22 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.taskfrog", appContext.packageName)
+    }
+
+    @Test
+    fun addList() {
+        onView(withId(R.id.list_fab)).perform(click())
+        onView(withId(R.id.listName)).perform(typeText("ListNameTest"), closeSoftKeyboard())
+        onView(withText("Ok")).perform(click())
+        Thread.sleep(3000)
     }
 }
