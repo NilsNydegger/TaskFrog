a851cc55ccd667dae7b7b0bd494b8a65fd3efa34
NilsNydegger <nils.nydegger@students.ffhs.ch>
2022-06-09 12:50:22 +0200

    gonna work on it on my other computer, temporary push

--- app/src/main/java/com/example/taskfrog/room/FrogTaskViewModel.kt
+++ app/src/main/java/com/example/taskfrog/room/FrogTaskViewModel.kt
@@ -2,18 +2,14 @@ package com.example.taskfrog.room
 
 import android.app.Application
 import androidx.lifecycle.*
-import com.example.taskfrog.ui.list.TaskFragment
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
-import java.time.LocalDate
-import java.util.*
 
 class FrogTaskViewModel(application: Application): AndroidViewModel(application){
 
     private val frogTaskRepository: FrogTaskRepository
     private var listId: Int = 0
-    private var frogDate: String = "12.12.2000"
+    private var tempFrogDate: String = "12.12.2000"
     var getAllTasks: LiveData<List<FrogTask>>? = null
     var getAllTasksFromDate: List<FrogTask> = emptyList()
     //This gets instantly initialized, just by our own initialisation - bad practice but temp fix
@@ -46,15 +42,7 @@ class FrogTaskViewModel(application: Application): AndroidViewModel(application)
     }
 
     fun dateInitialize(date: String){
-        frogDate = date
-        getAllTasksFromDate = frogTaskRepository.getFrogTasksFromDate(frogDate)
-    }
-
-    private fun updateDateInView(frogDate: LocalDate): String {
-        val date: String
-        val myFormat = "dd.MM.yyyy"
-        val sdf = SimpleDateFormat(myFormat, Locale.GERMAN)
-        date = sdf.format(frogDate)
-        return date
+        tempFrogDate = date
+        getAllTasksFromDate = frogTaskRepository.getFrogTasksFromDate(tempFrogDate)
     }
 }
--- app/src/main/java/com/example/taskfrog/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/taskfrog/ui/calendar/CalendarFragment.kt
@@ -12,13 +12,18 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.example.taskfrog.R
 import com.example.taskfrog.databinding.FragmentCalendarBinding
+import com.example.taskfrog.room.FrogTaskViewModel
+import java.text.SimpleDateFormat
+import java.time.LocalDate
 import java.util.*
 
 class CalendarFragment : Fragment() {
 
     private var _binding: FragmentCalendarBinding? = null
+    private lateinit var frogTaskViewModel: FrogTaskViewModel
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -29,22 +34,53 @@ class CalendarFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(CalendarViewModel::class.java)
+        frogTaskViewModel = ViewModelProvider(this)[FrogTaskViewModel::class.java]
 
         _binding = FragmentCalendarBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
         val calendarView = view?.findViewById<CalendarView>(R.id.calendarView)
 
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         //TODO: This
         binding.calendarView.setOnDateChangeListener{
-            view, year, month, dayOfMonth ->
-            val date = dayOfMonth.toString() + "." + (month + 1).toString() + "." + year.toString()
-            Toast.makeText(this.requireContext(), date, Toast.LENGTH_SHORT).show()
+                view, year, month, dayOfMonth ->
+            val date: String = createDateString(year, month, dayOfMonth)
+            Toast.makeText(this.requireContext(), date, Toast.LENGTH_SHORT).show() //TODO: Remove that in the end
+            fillRecyclerView(date)
         }
+    }
 
-        return root
+    private fun fillRecyclerView(date: String){
+        //frogTaskViewModel.dateInitialize(date)
+        //TODO: Have the List fill the recyclerview with adapter and above command
+        val recyclerview = view?.findViewById<RecyclerView>(R.id.calendarRecyclerView)
+        val calendarAdapter =
+    }
+
+    private fun createDateString(year: Int, month: Int, dayOfMonth: Int): String{
+        //Assigning Values is not very nice but it makes the code more understandable overall
+        var dayOfMonthString = "12"
+        var monthString = "12"
+        var yearString = "2000"
+
+        if(dayOfMonth < 10){
+            dayOfMonthString = "0$dayOfMonth"
+        } else {
+            dayOfMonthString = "$dayOfMonth"
+        }
+        if(month < 10){
+            monthString = "0$month"
+        } else {
+            monthString = "$month"
+        }
+        yearString = "$year"
+
+        return "$dayOfMonthString.$monthString.$yearString"
     }
 
     override fun onDestroyView() {
